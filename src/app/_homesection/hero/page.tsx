ddbe8088cc1ddf3f7911515413d59c78ce367a5b
web-yash <yashvaghasiya2411@gmail.com>
2025-03-24 21:16:28 +0530

    Remove some elements and add some design effect

--- src/app/_homesection/hero/page.tsx
+++ src/app/_homesection/hero/page.tsx
@@ -41,7 +41,7 @@ const page = () => {
             transition-all duration-300"
         />
         <div className="flex flex-col items-center justify-center gap-3 md:gap-4 px-4 py-4 mb-20 md:py-6">
-          <div className="flex items-center justify-center w-full">
+          {/* <div className="flex items-center justify-center w-full">
             <Image
               src="/homeSvg/image1.svg"
               alt="Hero illustration"
@@ -69,10 +69,10 @@ const page = () => {
             <h1 className="text-xl md:text-2xl font-semibold mt-[5px] text-dark dark:text-white">
               18.2K
             </h1>
-          </div>
+          </div> */}
           <div className="max-w-5xl px-2 mt-2">
             <h1 className="text-4xl md:text-5xl lg:text-7xl font-bold text-center max-w-3xl text-dark dark:text-white">
-              Your ultimate social media scheduling tool
+              Crafting Web Wonders & Gaming Glory, End-to-End!  
             </h1>
             <Image
               src="/homeSvg/drawing.svg"
@@ -85,8 +85,7 @@ const page = () => {
           <Icons />
           <div className="max-w-3xl px-4 mt-2">
             <p className="text-lg md:text-xl text-center font-semibold text-dark dark:text-white">
-              Postiz offers everything you need to manage your social media
-              posts, build an audience, capture leads, and grow your business.
+              Our agency delivers all you need for stunning websites, immersive games, and business growt - full stack, full impact.
             </p>
           </div>
           <div>
@@ -97,7 +96,7 @@ const page = () => {
               origin-center text-base md:text-lg font-semibold
               w-full md:w-auto"
             >
-              Start a 7-day trial&nbsp;&nbsp;&gt;&gt;
+              Ask Us Anything&nbsp;&nbsp;&gt;&gt;
             </button>
           </div>
         </div>
